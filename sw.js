be281cae042f447c93cb39252c30be3c17534fe9
KeyroYoa <ferreira.s.joaopaulo@gmail.com>
2026-09-18 12:09:48 +0100

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'gestor-tvde-v4';
+const FICHEIROS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FICHEIROS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});2
